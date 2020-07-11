d8caf676f6f4b2d4ab35f06ee8e8a3b96a0f49b8
Goodnews Sandy <goodnewssandy@gmail.com>
2020-07-11 20:07:07 +0100

    updated router set up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Gallery from './Components/Gallery';
+import Gallery from './components/Gallery';
 
 function App() {
   return (
